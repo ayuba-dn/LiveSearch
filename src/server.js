c72ccf5c6868f5fa65045fe63b58b6f538a47f33
Ayuba <ayubadau1994@gmail.com>
2020-09-20 07:47:34 -0700

    Initialization

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = ['Ayuba',"Musa","Hyedima","Moses"];
+
+app.use(express.static(__dirname + "/www"));
+
+app.get("/users", (req, res) => {
+  let searchText = req.query.searchText;
+  let result = users.filter(obj=>{
+    return obj.startsWith(searchText);
+  })
+  res
+    .status(200)
+    .json({payload:result});
+});
+
+app.listen(3000, () => console.log("Server Running on Port 3000"));
+
